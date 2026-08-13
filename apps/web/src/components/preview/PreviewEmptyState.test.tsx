4b2aa3ccb35fbe3536eeb5948617951889a9adbb
Jake Leventhal <jakeleventhal@me.com>
2026-08-13 15:54:01 -0400

    test(preview): preserve discovered server helpers

--- apps/web/src/components/preview/PreviewEmptyState.test.tsx
+++ apps/web/src/components/preview/PreviewEmptyState.test.tsx
@@ -15,7 +15,8 @@ const mocks = vi.hoisted(() => ({
   }>,
 }));
 
-vi.mock("./useDiscoveredLocalServers", () => ({
+vi.mock("./useDiscoveredLocalServers", async (importOriginal) => ({
+  ...(await importOriginal<typeof import("./useDiscoveredLocalServers")>()),
   useDiscoveredLocalServers: () => mocks.servers,
 }));
 vi.mock("./PreviewFaviconIcon", () => ({
